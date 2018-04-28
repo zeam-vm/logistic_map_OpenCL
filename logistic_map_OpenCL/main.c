cec12dffd21c162063c668e155fe6783708629fa
Susumu Yamazaki <zacky1972@gmail.com>
2018-04-28 10:12:00 +0900

    Initial Commit

---
+++ logistic_map_OpenCL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  logistic_map_OpenCL
+//
+//  Created by 山崎進 on 2018/04/28.
+//  Copyright © 2018年 山崎進. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
